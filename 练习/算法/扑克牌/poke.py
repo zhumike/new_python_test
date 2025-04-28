d939c69d34abd5c6255be4577fe7594d200deb92
zhenzhongzhu <zhuzhenzhong@yuewen.com>
2025-04-28 18:21:33 +0800

    更新gitignore   new2

--- 练习/算法/扑克牌/poke.py
+++ 练习/算法/扑克牌/poke.py
@@ -57,4 +57,4 @@ def is_straight_flush(cards):
     return is_consecutive(values) or values == [2, 3, 4, 5, 14]  # 处理 A-2-3-4-5
 
 print(is_straight_flush(['♠10', '♠J', '♠Q', '♠K', '♠A']))
-#test1
+#test1  new
